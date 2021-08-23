76a6c9312f1dcc0104ddd77c9b41054f816a2875
raxgomez <rafa_gomez24@hotmail.com>
2021-08-23 18:08:04 -0500

    ProBuilder

---
+++ Assets/Scripts/MovimientoPersonaje.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoPersonaje : MonoBehaviour
+{
+    CharacterController controller;
+
+    public float velocidadCaminar = 6f;
+    public float velocidadCorrer = 10f;
+    public float velocidadSalto = 8f;
+    public float Gravedad = 15f;
+
+    private Vector3 movimiento = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(controller.isGrounded)
+        {
+            movimiento = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+
+            if(Input.GetKey(KeyCode.LeftShift))
+            {
+                movimiento = transform.TransformDirection(movimiento) * velocidadCorrer;
+            } else
+                movimiento = transform.TransformDirection(movimiento) * velocidadCaminar;
+        }
+        movimiento.y -= Gravedad * Time.deltaTime;
+
+        controller.Move(movimiento * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Personaje.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personaje : MonoBehaviour
+{
+    public float velocidadMouse = 100f;
+    public Transform personaje;
+    float Rotacionx = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float valorx = Input.GetAxis("Mouse X") * velocidadMouse * Time.deltaTime;
+        float valory = Input.GetAxis("Mouse Y") * velocidadMouse * Time.deltaTime;
+
+        Rotacionx -= valory;
+        Rotacionx = Mathf.Clamp(Rotacionx, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(Rotacionx, 0f, 0f);
+        personaje.Rotate(Vector3.up * valorx);
+    }
+}
